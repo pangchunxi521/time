5cf8275e3e90d5a2cdc4ed38d43422651da0a30f
Promise <15022300258@163.com>
2021-11-25 15:07:26 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,2 +1,24 @@
-import time
-print(time.time())
+function tz(n){
+      return (n<10)?'0'+n:''+n
+  };
+
+  (function Rtime(){
+      var nowTime = new Date;
+      var endTime = new Date('2022/2/1 00:00:00');
+
+      var t = endTime.getTime()  - nowTime.getTime() ;
+      //alert(t)
+      if(t>=0){
+          var d = Math.floor(t/1000/60/60/24),
+              h = Math.floor(t/1000/60/60%24),
+              m = Math.floor(t/1000/60%60),
+              s = Math.floor(t/1000%60)
+      };
+
+       setInterval(Rtime,1000);
+
+       footer.innerHTML = '&copy <span class="time">探针小站</span> | 距离过年还有<span class="times">'+tz(d)+'</span>天<span class="time1">'+tz(h)+'</span>时<span class="time2">'+tz(m)+'</span>分<span class="time3">'+tz(s)+'</span>秒';;
+
+  })()
+
+
